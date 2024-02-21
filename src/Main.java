6e5200e45534aded17b61f69d959c220f303366b
REIGaruki <gyakujo@mail.ru>
2024-02-21 11:40:20 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        byte age = 34;//входные данные
+        boolean adulthood = (age >= 18);
+        if (adulthood) {
+            System.out.println("Возоаст человека равен " + age + ", он совершеннолетний");
+        } else {
+            System.out.println("Возоаст человека равен " + age +
+                    ", он не достиг совершеннолетия, нужно немного подождать");
+        }
     }
 }
