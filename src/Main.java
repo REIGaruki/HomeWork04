f2603457f521479ccafa89264ef4a2ec0d733ba3
REIGaruki <gyakujo@mail.ru>
2024-02-21 11:41:06 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,16 @@ public class Main {
         } else {
             System.out.println("Сегодня на улице " + temperature + " °C, нужно надеть шапку");
         }
-
+        //task 3
+        short speed = 80;//входные данные
+        boolean violation = (speed > 60);
+        if (violation) {
+            System.out.println("Если скорость " + speed+ " км/ч, то придется заплатить штраф");
+        }
+        speed = 60;//входные данные
+        violation = speed > 60;//приходится писать во второй раз для нового значения переменной speed
+        if (!violation) {
+            System.out.println("Если скорость " + speed+ " км/ч, то можно ездить спокойно");
+        }
     }
 }
